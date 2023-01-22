29241c0dd3285504fc0d7be2399a62180ae651c8
dmassta <d.sargaev@gmail.com>
2023-01-22 16:37:02 +0800

    added 3 fields (alphabet, alphabetUpperCase, alphabetSigns) and 3 get methods respectively in Alphabet.java

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,3 +1,21 @@
+import java.util.Locale;
+
 public class Alphabet {
     protected static final String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+    protected static final String alphabetUpperCase = alphabet.toUpperCase();
+
+    protected static final char[] alphabetSigns = new char[] {'.', ',', '"', '?', '!', '-', ':', ';', ' '};
+
+    protected static String getAlphabet() {
+        return alphabet;
+    }
+
+    protected static String getAlphabetUpperCase() {
+        return alphabetUpperCase;
+    }
+
+    protected static char[] getAlphabetSigns() {
+        return alphabetSigns;
+    }
 }
